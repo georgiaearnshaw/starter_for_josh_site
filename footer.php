babc01582fb5ce24183131a8af525d3352fb1861
georgiaearnshaw <61772678+georgiaearnshaw@users.noreply.github.com>
2020-03-18 10:46:38 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="container-fluid">
+      <div class="container">
+          <div class="row">
+              
+              <div class="col-md-6 text-center footer-content">
+               <p><?php bloginfo('name'); ?></p>
+                  <img src="file:///Users/localadmin/Documents/starter_for_josh_site/images/twitter.png" alt="twitter">
+                  <img src="file:///Users/localadmin/Documents/starter_for_josh_site/images/wifi.png" alt="twitter">
+              </div>
+                 <div class="col-md-6 text-center footer-content">
+               <p>Terms and Conditions</p>
+                      <img src="file:///Users/localadmin/Documents/starter_for_josh_site/images/in.png" alt="twitter">
+                  <img src="file:///Users/localadmin/Documents/starter_for_josh_site/images/facebook.png" alt="twitter">
+              </div>
+
+          </div>
+          </div>
+          
+      </footer>
+      
+      
+
+  </body>
+</html>
+<?php wp_footer(); ?>
