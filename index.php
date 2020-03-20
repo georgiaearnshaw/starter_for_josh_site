a017202c9e2e21084b8fc949d72b70a6775e3913
georgiaearnshaw <61772678+georgiaearnshaw@users.noreply.github.com>
2020-03-20 14:11:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -54,6 +54,11 @@
       
       <div class="container">
           
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            
+        
 <?php
 global $more;//define a global variable
 $more = 0;// the global varibale is now equal to 0
@@ -61,8 +66,19 @@ query_posts('cat=2');//look for posts that have the category of 2
 if(have_posts()) ://if we have posts to display
 while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
 ?>
-          
-          <div class="blocker"></div>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>
+    	
+ <div class="blocker"></div>
+            
+      </div><!--  container-->
+    </section>
+       
+      
           
               </div>
 <!--          container-->
