058012ba9853c6958bc6c64eacfa4c7a6bbf5e54
georgiaearnshaw <61772678+georgiaearnshaw@users.noreply.github.com>
2020-03-20 13:22:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -54,10 +54,13 @@
       
       <div class="container">
           
-          <h2>GAMES &amp; LEVEL DESIGNER</h2>
-          <p class="particle">and particle wizard</p>
-          <p class="about-text">My  name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
- I always tell a story.</p>
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=2');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
           
           <div class="blocker"></div>
           
